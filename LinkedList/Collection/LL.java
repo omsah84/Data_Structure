6ffab850666fcb90247ec5ec99c710e3f270247e
OM SAH <huncyoms@gmail.com>
2023-06-29 13:41:02 -0400

    commit

--- LinkedList/Collection/LL.java
+++ LinkedList/Collection/LL.java
@@ -4,21 +4,42 @@ public class LL{
 
     public static void printLinst(LinkedList<String> list){
         for(int i =0;i<list.size();i++){
-            System.out.println(list.get(i));
+            System.out.print(list.get(i)+" ->");
+        }
+        System.out.println("null");
+    }
+
+    public static void delete(LinkedList<String> list, int postion){
+         
+        if(list.size()>=postion){
+            for(int i =0;i<list.size();i++){
+                if(i== postion-1){
+                    list.remove(i);
+                }
+
+            }
         }
     }
+
+
     public static void main(String[] args) {
         LinkedList<String> list = new LinkedList<String>();
 
 
-        list.addFirst("hell");
-        list.addFirst("hey");
+        // list.addFirst("hell");
+        // list.addFirst("hey");
 
-        list.addLast("om");
-        list.addLast("sah");
+        // list.addLast("om");
+        // list.addLast("sah");
 
 
-        System.out.println(list.size());
+        System.out.println("The size of linked list: "+list.size());
+        printLinst(list);
+
+        delete(list, 4);
+
+        printLinst(list);
+
 
 
     }
