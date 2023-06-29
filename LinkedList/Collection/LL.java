943f65f9f72defbc9572d746b226a66b5176ef81
OM SAH <huncyoms@gmail.com>
2023-06-29 13:28:12 -0400

    commit

---
+++ LinkedList/Collection/LL.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class LL{
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+
+        
+        System.out.println(list.size());
+
+
+    }
+}
