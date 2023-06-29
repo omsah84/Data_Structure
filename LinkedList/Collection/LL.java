79b61adc11df6ba4732f5e2b03488cacd5126f7d
OM SAH <huncyoms@gmail.com>
2023-06-29 13:31:39 -0400

    commit

--- LinkedList/Collection/LL.java
+++ LinkedList/Collection/LL.java
@@ -1,10 +1,23 @@
 import java.util.*;
 
 public class LL{
+
+    public static void printLinst(LinkedList<String> list){
+        for(int i =0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+    }
     public static void main(String[] args) {
         LinkedList<String> list = new LinkedList<String>();
 
-        
+
+        list.addFirst("hell");
+        list.addFirst("hey");
+
+        list.addLast("om");
+        list.addLast("sah");
+
+
         System.out.println(list.size());
 
 
